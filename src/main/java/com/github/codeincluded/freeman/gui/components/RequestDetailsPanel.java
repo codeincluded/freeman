c3ccd222d5cc433d880a2d1bd02b82e02bfe1912
codeincluded <codeincluded@gmail.com>
2023-09-12 00:00:15 +0300

    Expand dynamically the headers table.

--- src/main/java/com/github/codeincluded/freeman/gui/components/RequestDetailsPanel.java
+++ src/main/java/com/github/codeincluded/freeman/gui/components/RequestDetailsPanel.java
@@ -1,6 +1,8 @@
 package com.github.codeincluded.freeman.gui.components;
 
 import javax.swing.*;
+import javax.swing.event.TableModelEvent;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
@@ -8,11 +10,16 @@ import java.util.Map;
 public class RequestDetailsPanel extends JTabbedPane {
 
     private final JEditorPane bodyEditorPane = new JEditorPane();
-    private final JTable headersTable = new JTable(1, 2);
+    private final DefaultTableModel tableModel = new DefaultTableModel();
+    private final JTable headersTable = new JTable(tableModel);
 
     public RequestDetailsPanel() {
         addTab("Headers", createHeadersTabContent());
         addTab("Body", createBodyTabContent());
+
+        tableModel.setColumnCount(2);
+        tableModel.setColumnIdentifiers(new String[]{"Key", "Value"});
+        tableModel.setRowCount(1);
     }
 
     public String getRequestBody() {
@@ -33,6 +40,14 @@ public class RequestDetailsPanel extends JTabbedPane {
     }
 
     private JComponent createHeadersTabContent() {
+        tableModel.addTableModelListener(event -> {
+            int row = event.getFirstRow();
+            boolean isUpdate = event.getType() == TableModelEvent.UPDATE;
+            boolean isLastRow = (row + 1) == tableModel.getRowCount();
+            if (isUpdate && isLastRow) {
+                tableModel.addRow(new Object[]{"", ""});
+            }
+        });
         return new JScrollPane(headersTable);
     }
 
